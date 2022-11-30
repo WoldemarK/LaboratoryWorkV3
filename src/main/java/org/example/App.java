f4469f7be23df7f34a9fefcf59ff1d3f94796aa4
WoldemarK <kovtynov.vladimir@gmail.com>
2022-12-01 00:01:14 +0300

    School

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,33 +16,33 @@ public class App {
 
         try {
             Transaction transaction = session.beginTransaction();
-//            /**
-//             * С помощью Hibernate получите любого директора, а затем получите его
-//             * школу.
-//             */
-//            Principal somePrincipal = session.get(Principal.class, 1);
-//            System.out.println(somePrincipal);
-//
+            /**
+             * С помощью Hibernate получите любого директора, а затем получите его
+             * школу.
+             */
+            Principal somePrincipal = session.get(Principal.class, 1);
+            System.out.println(somePrincipal.getSchool().getSchoolNumber());
+
 //            /**
 //             * Получите любую школу, а затем получите ее директора.
 //             */
 //            School someSchool = session.get(School.class, 2);
-//            System.out.println(someSchool);
-
-            /**
-             * Создайте нового директора и новую школу и свяжите эти сущности.
-             */
-            Principal newPrincipal = new Principal("Ivan", 33);
-            School newSchool = new School(32, newPrincipal);
-
-            session.persist(newPrincipal);
-            session.persist(newSchool);
-
-            /**
-             * Смените директора у существующей школы.
-             */
-            Principal updatePrincipal = session.get(Principal.class, 3);
-            updatePrincipal.setName("Tobi");
+//            System.out.println(someSchool.getPrincipal().getName());
+//
+//            /**
+//             * Создайте нового директора и новую школу и свяжите эти сущности.
+//             */
+//            Principal newPrincipal = new Principal("Ivan", 33);
+//            School newSchool = new School(32, newPrincipal);
+//
+//            session.persist(newPrincipal);
+//            session.persist(newSchool);
+//
+//            /**
+//             * Смените директора у существующей школы.
+//             */
+//            Principal updatePrincipal = session.get(Principal.class, 3);
+//            updatePrincipal.setName("Tobi");
 
 
             transaction.commit();
